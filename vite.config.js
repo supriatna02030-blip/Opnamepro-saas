7be0ef964854a073406b28872e14c6eebd2b239c
supriatna02030-blip <supriatna02030@gmail.com>
2026-08-18 11:59:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'OpnamePro SaaS',
-        short_name: 'OpnamePro',
-        theme_color: '#4f46e5',
-        background_color: '#ffffff',
-        display: 'standalone'
-      }
-    })
-  ]
+  plugins: [react()],
+  base: '/Opnamepro-saas/', // INI YG PALING PENTING
 })
