f9ec2a039146925b8fc78a0cf280d9252845bd77
supriatna02030-blip <supriatna02030@gmail.com>
2026-08-18 11:30:02 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://wklzodxwzagmwxgpfzur.supabase.co', // GANTI PAKE PUNYA KAMU
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' // GANTI PAKE PUNYA KAMU
+  'https://wklzodxwzagmwxgpfzur.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrbHpvZHh3emFnbXd4Z3BmenVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5OTg1MzIsImV4cCI6MjEwMjU3NDUzMn0.ZRcCCjmG5XIJBZt6q_BN2cxwo1ijfzpRa-0Mr1UJykc'
 )
